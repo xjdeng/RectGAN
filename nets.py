c8f75cd998b3d17fb0bcec5d1a61c0420c742fe9
xjdeng <xjd001@gmail.com>
2019-01-20 22:11:02 -0600

    Add more layers

--- nets.py
+++ nets.py
@@ -32,70 +32,6 @@ factor2 = 1
 def build_enc( shape ) :
     return build_discriminator(shape, build_disc=False)
 
-def build_discriminator_old( shape, build_disc=True ) :
-    '''
-    Build discriminator.
-    Set build_disc=False to build an encoder network to test
-    the encoding/discrimination capability with autoencoder...
-    '''
-    def conv2d( x, filters, shape=(4, 4), **kwargs ) :
-        '''
-        I don't want to write lengthy parameters so I made a short hand function.
-        '''
-        x = Conv2D( filters, shape, strides=(2, 2),
-            padding='same',
-            kernel_initializer=Args.kernel_initializer,
-            **kwargs )( x )
-        #x = MaxPooling2D()( x )
-        x = BatchNormalization(momentum=Args.bn_momentum)( x )
-        x = LeakyReLU(alpha=Args.alpha_D)( x )
-        return x
-
-    # https://github.com/tdrussell/IllustrationGAN
-    # As proposed by them, unlike GAN hacks, MaxPooling works better for anime dataset it seems.
-    # However, animeGAN doesn't use it so I'll keep it more similar to DCGAN.
-
-    face = Input( shape=shape )
-    x = face
-
-    # Warning: Don't batchnorm the first set of Conv2D.
-
-    x = Conv2D( 64*factor2, (4, 4), strides=(2, 2),
-        padding='same',
-        kernel_initializer=Args.kernel_initializer)( x )
-    x = LeakyReLU(alpha=Args.alpha_D)( x )
-    # 32x32
-
-    x = conv2d( x, 128*factor2) #128
-    # 16x16
-    x = conv2d( x, 128*factor2)
-    x = conv2d( x, 256*factor2)
-
-    x = conv2d( x, 256*factor2) #256
-    # 8x8
-
-    x = conv2d( x, 512*factor2) #512
-    # 4x4
-    x = Dropout(Args.dropout)(x)
-    if build_disc:
-        x = Flatten()(x)
-        # add 16 features. Run 1D conv of size 3.
-        #x = MinibatchDiscrimination(16, 3)( x )
-
-        #x = Dense(1024, kernel_initializer=Args.kernel_initializer)( x )
-        #x = LeakyReLU(alpha=Args.alpha_D)( x )
-
-        # 1 when "real", 0 when "fake".
-        x = Dense(1, activation='sigmoid',
-            kernel_initializer=Args.kernel_initializer)( x )
-        return models.Model( inputs=face, outputs=x )
-    else:
-        # build encoder.
-        x = Conv2D(Args.noise_shape[2], (5,5) , activation='tanh')(x)
-        return models.Model( inputs=face, outputs=x )
-
-
-
 def build_discriminator( shape, build_disc=True ) :
     '''
     Build discriminator.
@@ -132,10 +68,10 @@ def build_discriminator( shape, build_disc=True ) :
 
     x = conv2d( x, 128*factor2) #128
     # 16x16
-
+    x = conv2d( x, 128*factor2)
     x = conv2d( x, 256*factor2) #256
     # 8x8
-
+    x = conv2d( x, 256*factor2)
     x = conv2d( x, 512*factor2) #512
     # 4x4
     x = Dropout(Args.dropout)(x)
@@ -153,75 +89,8 @@ def build_discriminator( shape, build_disc=True ) :
         return models.Model( inputs=face, outputs=x )
     else:
         # build encoder.
-        x = Conv2D(Args.noise_shape[2], (5,5) , activation='sigmoid')(x)
+        x = Conv2D(Args.noise_shape[2], (5,5) , activation='tanh')(x)
         return models.Model( inputs=face, outputs=x )
-
-
-
-def build_gen_old( shape ) :
-    def deconv2d( x, filters, shape=(5, 5), mystrides = (2, 2)) :
-        '''
-        Conv2DTransposed gives me checkerboard artifact...
-        Select one of the 3.
-        '''
-        # Simpe Conv2DTranspose
-        # Not good, compared to upsample + conv2d below.
-        x= Conv2DTranspose( filters, shape, padding='same',
-            strides=mystrides, kernel_initializer=Args.kernel_initializer )(x)
-
-        # simple and works
-        #x = UpSampling2D( (2, 2) )( x )
-        #x = Conv2D( filters, shape, padding='same' )( x )
-
-        # Bilinear2x... Not sure if it is without bug, not tested yet.
-        # Tend to make output blurry though
-        #x = bilinear2x( x, filters )
-        #x = Conv2D( filters, shape, padding='same' )( x )
-
-        x = BatchNormalization(momentum=Args.bn_momentum)( x )
-        x = LeakyReLU(alpha=Args.alpha_G)( x )
-        return x
-
-    # https://github.com/tdrussell/IllustrationGAN  z predictor...?
-    # might help. Not sure.
-
-    noise = Input( shape=Args.noise_shape )
-    x = noise
-    # 1x1x256
-    # noise is not useful for generating images.
-
-    x= Conv2DTranspose( 512*factor2, (4, 4),
-        kernel_initializer=Args.kernel_initializer )(x)
-    x = BatchNormalization(momentum=Args.bn_momentum)( x )
-    x = LeakyReLU(alpha=Args.alpha_G)( x )
-    x = deconv2d( x, 256*factor2, mystrides = (2,1))
-    x = deconv2d( x, 256*factor2, mystrides = (2,2))
-    x = deconv2d( x, 128*factor2, mystrides = (2,2))
-    x = deconv2d( x, 128*factor2, mystrides = (2,2))
-    x = deconv2d( x, 64*factor2, mystrides = (2,1))
-    # 4x4
-
-    #x = deconv2d( x, 256*factor2, mystrides = (4,2))
-    # 8x8
-
-    #x = deconv2d( x, 128*factor2, mystrides = (4,2) )
-    # 16x16
-
-    #x = deconv2d( x, 64*factor2 , mystrides = (2,2))
-    # 32x32
-
-    # Extra layer
-    x = Conv2D( 64*factor2, (3, 3), padding='same',
-        kernel_initializer=Args.kernel_initializer )( x )
-    x = BatchNormalization(momentum=Args.bn_momentum)( x )
-    x = LeakyReLU(alpha=Args.alpha_G)( x )
-    # 32x32
-    x = Dropout(Args.dropout)(x)
-    x= Conv2DTranspose( 3, (4, 4), padding='same', activation='tanh',
-        strides=(2, 2), kernel_initializer=Args.kernel_initializer )(x)
-    # 64x64
-
-    return models.Model( inputs=noise, outputs=x )
 
 
 def build_gen( shape ) :
@@ -247,8 +116,8 @@ def build_gen( shape ) :
         #x = Conv2D( filters, shape, padding='same' )( x )
 
         x = BatchNormalization(momentum=Args.bn_momentum)( x )
-        #x = LeakyReLU(alpha=Args.alpha_G)( x )
-        x = ReLU()(x)
+        x = LeakyReLU(alpha=Args.alpha_G)( x )
+        #x = ReLU()(x)
         return x
 
     # https://github.com/tdrussell/IllustrationGAN  z predictor...?
@@ -259,16 +128,17 @@ def build_gen( shape ) :
     # 1x1x256
     # noise is not useful for generating images.
 
-    x= Conv2DTranspose( 512*factor2, (4, 4),
+    x= Conv2DTranspose( 512*factor, (4, 4),
         kernel_initializer=Args.kernel_initializer )(x)
     x = BatchNormalization(momentum=Args.bn_momentum)( x )
-    #x = LeakyReLU(alpha=Args.alpha_G)( x )
-    x = ReLU()(x)
-    x = deconv2d( x, 256*factor2, mystrides = st[0])
-    x = deconv2d( x, 128*factor2, mystrides = st[1])
-    x = deconv2d( x, 64*factor2, mystrides = st[2])
+    x = LeakyReLU(alpha=Args.alpha_G)( x )
+    #x = ReLU()(x)
+    x = deconv2d( x, 256*factor,mystrides=(1,1))
+    x = deconv2d( x, 256*factor, mystrides = st[0])
+    x = deconv2d( x, 128*factor, mystrides = st[1])
+    x = deconv2d( x, 64*factor, mystrides = st[2])
     # 4x4
-
+    x = deconv2d( x, 64*factor,mystrides=(1,1))
     #x = deconv2d( x, 256*factor2, mystrides = (4,2))
     # 8x8
 
@@ -279,11 +149,11 @@ def build_gen( shape ) :
     # 32x32
 
     # Extra layer
-    x = Conv2D( 64*factor2, (3, 3), padding='same',
+    x = Conv2D( 64*factor, (3, 3), padding='same',
         kernel_initializer=Args.kernel_initializer )( x )
     x = BatchNormalization(momentum=Args.bn_momentum)( x )
-    #x = LeakyReLU(alpha=Args.alpha_G)( x )
-    x = ReLU()(x)
+    x = LeakyReLU(alpha=Args.alpha_G)( x )
+    #x = ReLU()(x)
     # 32x32
     x = Dropout(Args.dropout)(x)
     x= Conv2DTranspose( 3, (4, 4), padding='same', activation='tanh',
